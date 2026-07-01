1e4ad2f15973b9be7917431bc6241005a412ca2f
Ahmad Raza <safdarahmad249@gmail.com>
2026-07-01 23:22:52 +0500

    Refactor App component layout and styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,31 +20,41 @@ function App() {
 
   if (isCheckingAuth) return <PageLoader />;
 
-  return (
-    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
-      {/* DECORATORS - GRID BG & GLOW SHAPES */}
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
-      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
-      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+return (
+  <div className="min-h-screen bg-slate-900 relative overflow-hidden">
+
+    {/* Background */}
+    <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+
+    <div className="absolute top-0 -left-4 w-96 h-96 bg-pink-500 opacity-20 blur-[100px]" />
+
+    <div className="absolute bottom-0 -right-4 w-96 h-96 bg-cyan-500 opacity-20 blur-[100px]" />
+
+    {/* Main */}
+    <div className="relative w-full h-screen md:flex md:items-center md:justify-center md:p-4">
 
       <Routes>
         <Route
           path="/"
           element={authUser ? <ChatPage /> : <Navigate to="/login" />}
         />
+
         <Route
           path="/login"
           element={!authUser ? <LoginPage /> : <Navigate to="/" />}
         />
+
         <Route
           path="/signup"
           element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
         />
       </Routes>
 
-      <Toaster />
     </div>
-  );
-}
+
+    <Toaster />
+
+  </div>
+);
 
 export default App;
