60b335613cec84599afe24179eba7734e1bdbdec
Ahmadraza49 <safdarahmad249gmail.com>
2026-07-01 17:14:04 +0500

    update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,13 +10,12 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, JWT_SECRET, {
     expiresIn: "7d",
   });
-
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
-  });
+res.cookie("jwt", token, {
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "none",
+  secure: true,
+});
 
   return token;
 };
