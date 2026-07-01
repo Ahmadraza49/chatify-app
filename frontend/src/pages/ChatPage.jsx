8d26685b5f26dda8a0a63f55b873d86fef48c251
Ahmad Raza <safdarahmad249@gmail.com>
2026-07-01 23:05:42 +0500

    Refactor ChatPage layout for improved responsiveness

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -12,10 +12,14 @@ function ChatPage() {
   const { activeTab, selectedUser } = useChatStore();
 
   return (
-    <div className="relative w-full max-w-6xl h-[800px]">
-      <BorderAnimatedContainer>
+   <div className="relative w-full h-screen max-w-7xl">
+     <BorderAnimatedContainer className="flex flex-col md:flex-row h-full">
         {/* LEFT SIDE */}
-        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+       <div
+  className={`${
+    selectedUser ? "hidden md:flex" : "flex"
+  } w-full md:w-80 bg-slate-800/50 backdrop-blur-sm flex-col`}
+>
           <ProfileHeader />
           <ActiveTabSwitch />
 
@@ -25,7 +29,11 @@ function ChatPage() {
         </div>
 
         {/* RIGHT SIDE */}
-        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+       <div
+  className={`${
+    !selectedUser ? "hidden md:flex" : "flex"
+  } flex-1 flex-col bg-slate-900/50 backdrop-blur-sm`}
+>
           {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
         </div>
       </BorderAnimatedContainer>
